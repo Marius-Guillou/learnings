4c0ce8d8f26a00776caeaa88008cb3b7be6b4389
Marius.Guillou <marius.guillou@gmail.com>
2025-07-07 21:23:06 +0200

    première fonction

--- RotorPy.py
+++ RotorPy.py
@@ -1,2 +1,26 @@
-RotorPy
+#Importations
+import math 
 
+#Définition des paramètres
+T = 100 #Poussée (N)
+R = 0 #Rayon (m)
+Vrot = 0 #Vitesse de rotation (tr/min)
+Nb = 6 #Nombre de pales
+rho = 1.225 #Masse volumique air
+pi = 3.14159
+
+#Définitions des lois variables
+def Theta(r, omega): #Theta angle de calage (degrés), on définit la loi de vrillage ici
+    return 8
+
+def c(r, omega): #c corde de l'élément de pale (mm), on définit sa loi de variation 
+    return 40
+
+#Elément de pale
+
+
+#Grandeur moyenne
+def Vi_moy(T, R, rho):
+    return sqrt(T/(2*rho*pi*R**2))
+
+print(Vi_moy(T, R, rho))
