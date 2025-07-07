091b6fb0d2cffc64b0c11b4f75793de3aadf6a23
Marius.Guillou <marius.guillou@gmail.com>
2025-07-07 22:25:46 +0200

    Premier tracé

--- RotorPy.py
+++ RotorPy.py
@@ -1,8 +1,6 @@
 #Importations
-from math import sqrt
-from math import cos
-from math import acos
-from math import sin
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Définition des paramètres
 T = 100 #Poussée (N)
@@ -14,28 +12,36 @@ pi = 3.14159
 
 #Définitions des lois variables
 def Theta(r): #Theta angle de calage (degrés), on définit la loi de vrillage ici
-    return 8
+    return pi/12
 
 def c(r): #c corde de l'élément de pale (mm), on définit sa loi de variation 
-    return 40
+    return 0.040
 
 def Cz(alpha): #Cz en fonction de alpha, à partir de la polaire du profil
     return alpha
 
 #Elément de pale
 def Poussee_elmt(r):
-    Beta = acos(Omega*r/sqrt((Omega*r)**2 + Vi_moy*2)) #Angle d'arrivée du flux par rapport au plan de rotation
-    alpha = Theta - Beta #Angle d'attaque
-    dF = 0.5*rho*c(r)*(Vi_moy**2 + (Omega*r)**2)*Cz(alpha)*sin(Beta) #Poussée élémentaire
+    Beta = np.arccos(Omega*r/np.sqrt((Omega*r)**2 + Vi_moy**2)) #Angle d'arrivée du flux par rapport au plan de rotation
+    alpha = Theta(r) - Beta #Angle d'attaque
+    dF = 0.5*rho*c(r)*(Vi_moy**2 + (Omega*r)**2)*Cz(alpha)*np.cos(Beta) #Poussée élémentaire
     return dF 
 
 def Trainee_elmt(r):
-    Beta = acos(Omega*r/sqrt((Omega*r)**2 + Vi_moy*2)) #Angle d'arrivée du flux par rapport au plan de rotation
-    alpha = Theta - Beta #Angle d'attaque
-    dT = 0.5*rho*c(r)*(Vi_moy**2 + (Omega*r)**2)*Cz(alpha)*cos(Beta) #Poussée élémentaire
+    Beta = np.arccos(Omega*r/np.sqrt((Omega*r)**2 + Vi_moy**2)) #Angle d'arrivée du flux par rapport au plan de rotation
+    alpha = Theta(r) - Beta #Angle d'attaque
+    dT = 0.5*rho*c(r)*(Vi_moy**2 + (Omega*r)**2)*Cz(alpha)*np.sin(Beta) #Poussée élémentaire
     return dT 
 
 #Grandeur moyenne
-Vi_moy = sqrt(T/(2*rho*pi*R**2)) #Vitesse induite moyenne (m/s)
-P_moy = sqrt(T**3/(2*rho*pi*R**2)) #Puissance moyenne requise (W)
+Vi_moy = np.sqrt(T/(2*rho*pi*R**2)) #Vitesse induite moyenne (m/s)
+P_moy = np.sqrt(T**3/(2*rho*pi*R**2)) #Puissance moyenne requise (W)
 print("Vi_moy = ", Vi_moy,", Pi_moy = ", P_moy) #Affichage des valeurs
+
+#Tracés
+
+x = np.linspace(0.01, R, 500)
+y = Poussee_elmt(x)
+
+plt.plot(x, y, label="poussée élémentaire")
+plt.show()
